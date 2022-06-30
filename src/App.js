2fbe6d42804f848f4ae90b05c4ec7d5b864ab2e0
Ashok Sah <ashok.stelmec@gmail.com>
2022-06-30 13:44:37 +0530

    add modal in the form element

--- src/App.js
+++ src/App.js
@@ -1,23 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import PureModal from "react-pure-modal";
+import "react-pure-modal/dist/react-pure-modal.min.css";
 
 function App() {
+  const [user, setUser] = useState({
+    userName: "",
+    age: "",
+  });
+
+  const [display, setDisplay] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [modal, setModal] = useState(false);
+
+  const { userName, age } = user;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (e) => {
+    setUser((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleCancel = (e) => {
+    setModal(false);
+  };
+
+  const handleClick = (e) => {
+    
+    if (userName < 1 || age < 1) {
+      setIsError(true);
+      setModal(true);
+    } else {
+      setDisplay((prev) => [...prev, user]);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <form onSubmit={handleSubmit}>
+        <div className="form-label">
+          <h4>
+            <label>Username</label>
+          </h4>
+        </div>
+        <div className="form-control">
+          <input
+            type="text"
+            name="userName"
+            defaultValue={userName}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-label">
+          <h4>
+            <label>Age</label>
+          </h4>
+        </div>
+        <div className="form-control">
+          <input
+            type="number"
+            name="age"
+            defaultValue={age}
+            onChange={handleChange}
+          />
+        </div>
+        <br />
+        <button onClick={handleClick}>Add User</button>
+        <br />
+        <br />
+        {JSON.stringify(user)}
+      </form>
+      <div className="form-display">
+        <h4>{JSON.stringify(display)}</h4>
+        {display.map((item) => {
+          return (
+            <p>
+              {item.userName} ({item.age} years old)
+            </p>
+          );
+        })}
+      </div>
+      <div className="form-error">
+        {isError && <h2> Please Enter a Valid Input</h2>}
+      </div>
+      <div className="modal">
+        <PureModal
+          header="Your header"
+          footer={
+            <div>
+              <button onClick={handleCancel}>Cancel</button>
+              <button>Save</button>
+            </div>
+          }
+          isOpen={modal}
+          // closeButton="Close"
+          // closeButton={
+          //   <div>
+          //     <button>Close</button>
+          //   </div>
+          // }
+          closeButtonPosition="header"
+          onClose={() => {
+            setModal(false);
+            return true;
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <p>Your content</p>
+        </PureModal>
+      </div>
+      ;
     </div>
   );
 }
